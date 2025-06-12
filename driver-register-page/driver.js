e7f7ba105e77736c15a31723436ff59dc508105c
unknown <isobednlemchukwu@gmai.com>
2025-06-12 02:21:24 -0700

    added swiper js

--- driver-register-page/driver.js
+++ driver-register-page/driver.js
@@ -1,23 +1,15 @@
 
-  const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-
-    // If you want pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-
-    // If you want navigation buttons
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    // Autoplay (optional)
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-  });
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      hashNavigation: {
+        watchState: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
